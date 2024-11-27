8c1fce397cafa16c38e53484bec6c54f816ed182
Jim Pivarski <jpivarski@gmail.com>
2024-11-27 17:00:13 -0600

    add a script to convert all markdown to notebooks

---
+++ make-notebooks.sh
@@ -0,0 +1,9 @@
+mkdir -p notebooks
+
+cd deep-learning-intro-for-hep
+
+for x in *.md; do y=`echo $x | sed s/.md/.ipynb/`; jupytext --to notebook $x -o ../notebooks/$y --update-metadata '{"kernelspec": {"display_name": "Python 3 (ipykernel)", "language": "python", "name": "python3"}}'; done
+
+cp -a img ../notebooks/img
+
+cd ..
